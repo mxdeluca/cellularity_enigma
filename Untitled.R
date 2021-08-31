cb032c462a97d7ed09b3bd399ad9f19f30a95ec8
mxdeluca <37563357+mxdeluca@users.noreply.github.com>
2021-08-31 18:34:12 +1000

    Delete Untitled.R

--- Untitled.R
+++
@@ -1,7 +0,0 @@
-install.packages("usethis")
-library(usethis)
-usethis::use_git_config(user.name = "mxdeluca",
-                        user.email = "m.deluca@uq.edu.au")
-credentials::set_github_pat("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")
-usethis::edit_r_environ()
-usethis::git_sitrep()
